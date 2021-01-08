5dd5f0051bf41429a55f58e638c5e3d17f23181c
Eskimon <eskimon@outlook.com>
2021-01-08 15:21:37 +0100

    Fix wrapping around map

--- flights.js
+++ flights.js
@@ -35,22 +35,42 @@ flights.push({
       fillOpacity: 0,
     }
 
-    this.marker[0] = L.marker([this.flight['DepartureAirport']['Latitude'], this.flight['DepartureAirport']['Longitude']], {icon: takeOffIcon}).addTo(mymap);
-    this.marker[1] = L.marker([this.arrival['Latitude'], this.arrival['Longitude']], {icon: landingIcon}).addTo(mymap);
-    this.pointMarker[0] = L.circleMarker([this.flight['DepartureAirport']['Latitude'], this.flight['DepartureAirport']['Longitude']], pointOption).addTo(mymap);
-    this.pointMarker[1] = L.circleMarker([this.arrival['Latitude'], this.arrival['Longitude']], pointOption).addTo(mymap);
+    let poly = [[this.flight['DepartureAirport']['Latitude'], this.flight['DepartureAirport']['Longitude']]];
+    let lastLon = this.flight['DepartureAirport']['Longitude'];
+    let wrap = false;
+    for(let i=0; i < this.points.length; i++) {
+      if((lastLon < 0 && this.points[i]['Aircraft']['Longitude'] > 0 && Math.abs(this.points[i]['Aircraft']['Longitude'] > 150)) || 
+         (lastLon > 0 && this.points[i]['Aircraft']['Longitude'] < 0 && Math.abs(this.points[i]['Aircraft']['Longitude'] > 150))) {
+        wrap = true
+      }
+      let point = L.latLng(this.points[i]['Aircraft']['Latitude'], this.points[i]['Aircraft']['Longitude']);
+      if(wrap) {
+        point.lng += (point.lng < 0) ? 360 : -360
+      }
+      poly.push(point);
+      lastLon = this.points[i]['Aircraft']['Longitude'];
+    }
+    let lastpoint = L.latLng(this.arrival['Latitude'], this.arrival['Longitude']);
+    if(wrap) {
+      lastpoint.lng += (lastpoint.lng < 0) ? 360 : -360
+    }
+    poly.push(lastpoint)
+    this.polyline = L.polyline(poly, {color: this.color}).addTo(mymap);
+
+    let startLoc = L.latLng(this.flight['DepartureAirport']['Latitude'], this.flight['DepartureAirport']['Longitude'])
+    let stopLoc = L.latLng(this.arrival['Latitude'], this.arrival['Longitude'])
+    if(wrap) {
+      stopLoc.lng += (stopLoc.lng < 0) ? 360 : -360
+    }
+    this.marker[0] = L.marker(startLoc, {icon: takeOffIcon}).addTo(mymap);
+    this.marker[1] = L.marker(stopLoc, {icon: landingIcon}).addTo(mymap);
+    this.pointMarker[0] = L.circleMarker(startLoc, pointOption).addTo(mymap);
+    this.pointMarker[1] = L.circleMarker(stopLoc, pointOption).addTo(mymap);
 
     markerLayer.addLayer(this.marker[0]);
     markerLayer.addLayer(this.marker[1]);
     circleMarkerLayer.addLayer(this.pointMarker[0]);
     circleMarkerLayer.addLayer(this.pointMarker[1]);
-
-    let poly = [[this.flight['DepartureAirport']['Latitude'], this.flight['DepartureAirport']['Longitude']]];
-    for(let i=0; i < this.points.length; i++) {
-      poly.push([this.points[i]['Aircraft']['Latitude'], this.points[i]['Aircraft']['Longitude']]);
-    }
-    poly.push([this.arrival['Latitude'], this.arrival['Longitude']]);
-    this.polyline = L.polyline(poly, {color: this.color}).addTo(mymap);
   }
 
   buildListNode() {
